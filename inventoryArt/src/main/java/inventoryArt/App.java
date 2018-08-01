7fe214c1ba2acd59aff093713314ca0576f21233
RathnaVS <38005977+RathnaVS@users.noreply.github.com>
2018-08-02 00:24:20 +0530

    inventoryArt

---
+++ inventoryArt/src/main/java/inventoryArt/App.java
@@ -0,0 +1,12 @@
+package inventoryArt;
+
+public class App {
+	
+	public static void main(String[] args){
+		System.out.println("Hello Ramesh");
+	}
+
+	public int calc(){
+		return 0;
+	}
+}
---
+++ inventoryArt/src/test/java/inventoryArt/AppTest.java
@@ -0,0 +1,15 @@
+package inventoryArt;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void testApp()
+	{
+		assertEquals(0,new App().calc());
+	}
+
+	
+}
